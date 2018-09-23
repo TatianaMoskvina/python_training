aa3c5b0712a46a070498c3efec47b082f2345b06
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2018-09-23 14:19:56 +0700

    new test "add new address" with extract methods and with parameters + Parameter Object

---
+++ address.py
@@ -0,0 +1,15 @@
+class Address:
+    def __init__(self, first_name, midle_name, last_name, nick_name, company, addrs, home, mobile, work, fax,
+         email):
+        self.first_name = first_name
+        self.midle_name = midle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.company = company
+        self.addrs = addrs
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+
--- test_add_new.py
+++ test_add_new.py
@@ -6,20 +6,22 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from address import Address
+
 
 class AddNew(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_new(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_address(wd)
-        self.create_new_address(wd, FirstName="Fname", MidleName="Mname", LastName="Lname", NickName="NickName", Company="Ant", Address="123", Home="123", Mobile="123", Work="1234", Fax="12345",
-                                Email="a@a.com")
+        self.create_new_address(wd, Address(first_name="Fname", midle_name="Mname", last_name="Lname", nick_name="NickName",
+                                company="Ant", addrs="123", home="123", mobile="123", work="1234", fax="12345",
+                                email="a@a.com"))
         self.submit(wd)
         self.logout(wd)
 
@@ -31,47 +33,46 @@ class AddNew(unittest.TestCase):
         # submit address creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def create_new_address(self, wd, FirstName, MidleName, LastName, NickName, Company, Address, Home, Mobile, Work, Fax,
-                           Email):
+    def create_new_address(self, wd, address):
         # initial address creation
         wd.find_element_by_link_text("add new").click()
         # fill address form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(FirstName)
+        wd.find_element_by_name("firstname").send_keys(address.first_name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(MidleName)
+        wd.find_element_by_name("middlename").send_keys(address.midle_name)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(LastName)
+        wd.find_element_by_name("lastname").send_keys(address.last_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(NickName)
+        wd.find_element_by_name("nickname").send_keys(address.nick_name)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(Company)
+        wd.find_element_by_name("company").send_keys(address.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(Address)
+        wd.find_element_by_name("address").send_keys(address.addrs)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Home)
+        wd.find_element_by_name("home").send_keys(address.home)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(Mobile)
+        wd.find_element_by_name("mobile").send_keys(address.mobile)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(Work)
+        wd.find_element_by_name("work").send_keys(address.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(Fax)
+        wd.find_element_by_name("fax").send_keys(address.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(Email)
+        wd.find_element_by_name("email").send_keys(address.email)
 
     def open_add_new_address(self, wd):
         # open add new address
