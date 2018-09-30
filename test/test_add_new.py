f98638ccf85533ecc191619a99291ddf4112f9ee
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2018-09-30 14:22:56 +0700

    созданы пакеты для разных зон отвественности

--- test/test_add_new.py
+++ test/test_add_new.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
-
 import pytest
-from address import Address
-from application import Application
-
+from model.address import Address
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
     fixture = Application()
-    return fixture
     request.addfinalizer(fixture.destroy)
-
-
+    return fixture
 
 
 def test_add_new(app):
     app.login(username="admin", password="secret")
-    app.open_add_new_address()
-    app.create_new_address(Address(first_name="Fname", midle_name="Mname", last_name="Lname", nick_name="NickName",
+    open_add_new_address()
+    self.app.create_new_address(Address(first_name="Fname", midle_name="Mname", last_name="Lname", nick_name="NickName",
                                 company="Ant", addrs="123", home="123", mobile="123", work="1234", fax="12345",
                                 email="a@a.com"))
     app.submit()
     app.logout()
+
 
