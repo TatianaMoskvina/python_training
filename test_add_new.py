0c4d2ff520e1b08afa8c586e637483d356bdab21
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2018-09-30 10:26:15 +0700

    30.09 вынесли wd + Application + fixture

--- test_add_new.py
+++ test_add_new.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
+import pytest
 from address import Address
 from application import Application
 
 
-class AddNew(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    return fixture
+    request.addfinalizer(fixture.destroy)
 
 
-    def test_add_new(self):
-        self.app.login(username="admin", password="secret")
-        self.app.open_add_new_address()
-        self.app.create_new_address(Address(first_name="Fname", midle_name="Mname", last_name="Lname", nick_name="NickName",
+
+
+def test_add_new(app):
+    app.login(username="admin", password="secret")
+    app.open_add_new_address()
+    app.create_new_address(Address(first_name="Fname", midle_name="Mname", last_name="Lname", nick_name="NickName",
                                 company="Ant", addrs="123", home="123", mobile="123", work="1234", fax="12345",
                                 email="a@a.com"))
-        self.app.submit()
-        self.app.logout()
+    app.submit()
+    app.logout()
 
-
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-if __name__ == "__main__":
-    unittest.main()
