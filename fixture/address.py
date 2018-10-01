b0e72a220bf1ee3eb1da674386f99f85c81d1b52
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2018-10-01 23:25:07 +0700

    тест удаления группы + conftest

---
+++ fixture/address.py
@@ -0,0 +1,59 @@
+class AddressHelper:
+
+    def __init__(self,app):
+        self.app = app
+
+
+    def create(self, address):
+        wd = self.app.wd
+        self.open_add_new_address()
+        # initial address creation
+        wd.find_element_by_link_text("add new").click()
+        # fill address form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(address.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(address.midle_name)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(address.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(address.nick_name)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(address.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(address.addrs)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(address.home)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(address.mobile)
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(address.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(address.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(address.email)
+
+
+    def open_add_new_address(self):
+        wd = self.app.wd
+        # open add new address
+        wd.find_element_by_id("container").click()
+
+    def submit(self):
+        wd = self.app.wd
+        # submit address creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
--- test/test_add_new.py
+++ test/test_add_new.py
@@ -1,14 +1,5 @@
 
-import pytest
 from model.address import Address
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 
 def test_add_new(app):
     app.session.login(username="admin", password="secret")
